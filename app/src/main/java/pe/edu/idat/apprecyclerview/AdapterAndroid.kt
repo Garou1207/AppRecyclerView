c537c429ef9e88ebd18e16a01f0c7a38911a3b36
Garou1207 <kevinbarrutia@outlook.com>
2024-02-26 14:34:38 -0500

    Ejercicio terminado

--- app/src/main/java/pe/edu/idat/apprecyclerview/AdapterAndroid.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/AdapterAndroid.kt
@@ -1,22 +1,37 @@
 package pe.edu.idat.apprecyclerview
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import pe.edu.idat.apprecyclerview.databinding.ItemAndroidBinding
 
-class AdapterAndroid(val listAndroid: ArrayList<String>): RecyclerView.Adapter<AdapterAndroid.ViewHolder>(){
+class AdapterAndroid (val listAndroid: List<Android>, val context: Context)
+    : RecyclerView.Adapter<AdapterAndroid.ViewHolder>(){
 
     inner class ViewHolder(val binding: ItemAndroidBinding): RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemAndroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemAndroidBinding
+            .inflate(LayoutInflater.from(parent.context),parent,false)
         return ViewHolder(binding)
     }
-
     override fun getItemCount(): Int = listAndroid.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvNomVersion.text = listAndroid[position]
+        with(holder){
+            with(listAndroid[position]){
+                binding.tvNomVersion.text = nombre
+                binding.tvNumVersion.text = version
+                binding.ivAndroid.setImageResource(img)
+                binding.cvAndroid.setOnClickListener(View.OnClickListener {
+                    Toast.makeText(
+                        context,
+                        "Click Item Android $nombre",
+                        Toast.LENGTH_LONG).show()
+                })
+            }
+        }
     }
 }
--- app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
@@ -2,10 +2,40 @@ package pe.edu.idat.apprecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import pe.edu.idat.apprecyclerview.databinding.ActivityMainBinding
+import pe.edu.idat.apprecyclerview.databinding.ItemAndroidBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvAndroid.layoutManager = LinearLayoutManager(applicationContext)
+        binding.rvAndroid.adapter = AdapterAndroid(
+            listaAndroid(),applicationContext)
+
+        //binding.rvAndroid.layoutManager = StaggeredGridLayoutManager
+        //binding.rvAndroid.layoutManager = GridLayoutManager
     }
+
+    fun listaAndroid(): List<Android>{
+        var lista = ArrayList<Android>()
+        lista.add(Android("Angel Cake", "Android 1.0", R.drawable.image1))
+        lista.add(Android("Petite Four", "Android 1.1", R.drawable.image2))
+        lista.add(Android("Cupcake", "Android 1.5", R.drawable.image3))
+        lista.add(Android("Donut", "Android 1.6", R.drawable.image4))
+        lista.add(Android("Eclair", "Android 2.0 - 2.1", R.drawable.image5))
+        lista.add(Android("Froyo", "Android 2.2 - 2,2,3", R.drawable.image6))
+        lista.add(Android("Gingerbread", "Android 2.3 - 2.3.7", R.drawable.image7))
+        lista.add(Android("Honeycomb", "Android 3.0 - 3,2,6", R.drawable.image8))
+        lista.add(Android("Jelly Bean", "Android 4.1 - 4.3.1", R.drawable.image9))
+        lista.add(Android("Mashmellow", "Android 6.0 - 6.1", R.drawable.image10))
+        return lista
+    }
+
 }
